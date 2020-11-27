6fe24c86413453973b56729707eabcab8d425d8b
Michał Szopiński <mszopinski@mssnt.pl>
2020-11-27 14:39:03 +0100

    Implement Harmony patching

--- disco-translator-2/DiscoTranslator2.cs
+++ disco-translator-2/DiscoTranslator2.cs
@@ -2,16 +2,38 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using BepInEx;
+using HarmonyLib;
 
 namespace DiscoTranslator2
 {
-    [BepInPlugin("pl.mssnt.DiscoTranslator2", "Disco Translator 2", "0.1.0.0")]
+    [BepInPlugin("pl.mssnt.DiscoTranslator2", "Disco Translator 2", "1.0.0.0")]
     [BepInProcess("disco.exe")]
     class DiscoTranslator2 : BaseUnityPlugin
     {
         public DiscoTranslator2()
         {
-            Logger.LogInfo("Hello world");
+
         }
+
+        public void Awake()
+        {
+            //instantiate Harmony and patch over localization methods
+            Harmony harmony = new Harmony("pl.mssnt.DiscoTranslator2");
+            harmony.PatchAll();
+        }
+    }
+}
+
+[HarmonyPatch(typeof(I2.Loc.LocalizationManager), "GetTranslation")]
+class Patch
+{
+    //patch over the GetTranslation method of I2 Localization
+    public static bool Prefix(string Term, string overrideLanguage, ref string __result)
+    {
+        //abort translation if there is no term or the override language is english
+        if (Term == null) return true;
+        if (overrideLanguage != null && overrideLanguage == "English") return true;
+
+        return false;
     }
 }
